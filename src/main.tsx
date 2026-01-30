20ed05e5050ccb3e693cb452d95f25dea33050a6
Michael Kisilenko <mrkisilenko@gmail.com>
2026-01-30 12:48:30 +0200

    fix: add BrowserRouter to enable React Router context

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import { ErrorBoundary } from './components/ErrorBoundary'
@@ -8,6 +9,8 @@ import './lib/console-logger'
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ErrorBoundary>
 );
